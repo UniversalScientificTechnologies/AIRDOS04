03800d4d6b244244d247764603cd331116be14a2
kaklik <kaklik@users.noreply.github.com>
2025-08-27 14:19:45 +0000

    Revert "attemt to change uart settings for bootloader.", extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "f2f46893da1433df6fd851fca851b1719ac4a32d,User";
+String githash = "7d8300999ff3701e8687affbe3e018329d73431b,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
