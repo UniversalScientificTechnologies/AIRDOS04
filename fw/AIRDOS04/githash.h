f2d638be600a6be1788706e20017e67bd3971250
kaklik <kaklik@users.noreply.github.com>
2025-08-27 13:00:12 +0000

    Update filename in firmware updating command., extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "66e6db709a2c5e4c304628be5d467552021cafd3,User";
+String githash = "97048f6d3d56fd49d03f57733b069dacd49ddb64,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
