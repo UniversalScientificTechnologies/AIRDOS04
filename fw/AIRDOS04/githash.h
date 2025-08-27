5f1c8665247b3e29115d7c9ac350b14eefbadc00
kaklik <kaklik@users.noreply.github.com>
2025-08-27 12:27:05 +0000

    Remove build number from output hex filename., extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "d4d31b0ed77e01c6860324ec5541df72bd08d28c,User";
+String githash = "4340577ddbeb98a6107f7c002d2872c7731a2fc7,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
