5d4a1db1183707d1aa521dc118f91209aac6761d
kaklik <kaklik@users.noreply.github.com>
2025-08-27 13:27:58 +0000

    Switch Urboot bootloader to Optiboot., extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "97048f6d3d56fd49d03f57733b069dacd49ddb64,User";
+String githash = "f2f46893da1433df6fd851fca851b1719ac4a32d,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
