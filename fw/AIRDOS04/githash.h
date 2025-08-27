03fa1966acbb048e55725c1c9262b7650512d2c9
kaklik <kaklik@users.noreply.github.com>
2025-08-27 12:44:59 +0000

    Switch bootloader to UART1., extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "4340577ddbeb98a6107f7c002d2872c7731a2fc7,User";
+String githash = "66e6db709a2c5e4c304628be5d467552021cafd3,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
