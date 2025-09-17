fbdce0fa3335c2bc0d989911ed26f7ca0c1952c2
roman-dvorak <roman-dvorak@users.noreply.github.com>
2025-09-17 12:29:29 +0000

    Modify battery charger configuration in AIRDOS04X, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "7d8300999ff3701e8687affbe3e018329d73431b,User";
+String githash = "f4fee908bf6b54439607ccc75cfcd33c6f948fec,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
