5175be4c1d3ee9de981ab61af2fae1cdc9995988
roman-dvorak <roman-dvorak@users.noreply.github.com>
2025-12-09 14:40:06 +0000

    Detect battery presence and disable charger when absent, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "24aee6a42da37ed7ae151914e7ca7903047692a6,User";
+String githash = "0075b2e813aa006bf19a002661fd112de8bd9f35,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
