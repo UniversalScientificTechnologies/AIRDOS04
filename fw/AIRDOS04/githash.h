189b12039749e90a997f052e695ecbac759633c0
roman-dvorak <roman-dvorak@users.noreply.github.com>
2025-12-09 15:08:03 +0000

    Fix USB reader mode to not exit when battery absent, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "0075b2e813aa006bf19a002661fd112de8bd9f35,User";
+String githash = "82cc72800d389673ab9fa952b3379e375dd85546,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
