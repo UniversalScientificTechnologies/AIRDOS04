40ddb16189465661c97927d6336f661ceadc6c33
roman-dvorak <roman-dvorak@users.noreply.github.com>
2025-12-10 00:52:21 +0000

    Merge branch 'feature/battery-presence-detect' of github.com:UniversalScientificTechnologies/AIRDOS04 into feature/battery-presence-detect, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "f263776f331691e6b327f882448d187cd32f4c2d,User";
+String githash = "9b1c2987d57330f8027bca012a66356a259a050e,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
