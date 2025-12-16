4ce474ad27cbc7af510aab929a2787b224aa1401
roman-dvorak <roman-dvorak@users.noreply.github.com>
2025-12-16 19:47:50 +0000

    Přidána kontrola a inicializace RTC při startu, extended build, extended build, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "957288f7b2e6ff36481feb0dcd4a5e60c7b390b4,User";
+String githash = "7b07fbd678719bf66f71c76dbed50308b2e1967b,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
