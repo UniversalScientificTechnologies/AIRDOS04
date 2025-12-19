dedf019993258ef446969e3a307952ad34bb5f07
roman-dvorak <roman-dvorak@users.noreply.github.com>
2025-12-19 11:44:02 +0000

    Add RTC time reading and EEPROM sync time display, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "7b07fbd678719bf66f71c76dbed50308b2e1967b,User";
+String githash = "7aea278aa7b49160fa57a1f50c77bdd13717298c,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
