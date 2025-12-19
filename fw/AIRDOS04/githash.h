6259404c5a198f8243577ba9b4e50924c6867f60
roman-dvorak <roman-dvorak@users.noreply.github.com>
2025-12-19 12:20:37 +0000

    Add nextversion file for versioning information, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "7aea278aa7b49160fa57a1f50c77bdd13717298c,User";
+String githash = "a04a37ccf5ef6908732c4dbe46c915a75ab456ce,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
