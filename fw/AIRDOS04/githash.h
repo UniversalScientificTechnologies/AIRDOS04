710ad49f1fe1f5690575ea7bba64c0d1f27d3305
kaklik <kaklik@users.noreply.github.com>
2026-02-14 09:04:35 +0000

    Delete fw/AIRDOS04/build directory, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "a04a37ccf5ef6908732c4dbe46c915a75ab456ce,User";
+String githash = "89c0544a7660aaa543377b655503926e19492b08,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
