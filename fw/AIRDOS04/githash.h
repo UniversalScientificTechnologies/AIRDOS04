0b3aa5542937d5229dd6dc9995da18ed472ca5e5
roman-dvorak <roman-dvorak@users.noreply.github.com>
2026-03-02 20:00:28 +0000

    add PAT, extended build, extended build, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "fc1b3c803f3cc526d179f4d190329a89abd54503,User";
+String githash = "f9f50949ef19d41061e0ae1df4199a045c1f129d,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
