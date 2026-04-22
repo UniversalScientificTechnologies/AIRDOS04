c048050287d6d365f0184f33a8f7d7f5c62097aa
roman-dvorak <roman-dvorak@users.noreply.github.com>
2026-04-22 14:08:18 +0000

    Merge branch 'feature/log-header-fseq' of github.com:UniversalScientificTechnologies/AIRDOS04 into feature/log-header-fseq, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "6a9d21c953d65d9824decc75e3806124f0fb2224,User";
+String githash = "0824e03e89c50cca48c89cd6f78d630e8b35c1a7,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
