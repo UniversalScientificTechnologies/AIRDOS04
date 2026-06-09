b134751ecd4919ead3e545621e1050d51d4fcef5
slimonslimon <vithanousek@seznam.cz>
2026-06-09 18:24:59 +0000

    Merge remote-tracking branch 'origin/features/log-header-fseq-squashed' into features/log-header-fseq-squashed, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "bb11bca8627c0e7efdbca8054f3f0d0913dd7fbb,User";
+String githash = "402f2ad4e0c6c20feb29f778cab4d7f8b5b50269,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
