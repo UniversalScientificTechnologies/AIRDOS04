6aaae9aa2850b3c151d75cec46bc9eb760dabdf9
slimonslimon <vithanousek@seznam.cz>
2026-06-09 18:41:07 +0000

    Squashed fseq branch hopefully singed with action commit singed., extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "e375b67af30c502d3c9057d961f0da6bd5af1995,User";
+String githash = "619c9600a2742e4a39e760693fa1c5c68bdf3b66,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
